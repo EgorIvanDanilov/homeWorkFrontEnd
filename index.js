4f41413c0beec15fe3a0ad57ea2f4b60c0da061e
Egor Danilov <mellowd94@gmail.com>
2023-10-03 16:37:12 +0200

    add first home work

---
+++ index.js
@@ -0,0 +1,10 @@
+// let getElementAtIndex = (array, index) => {
+//    return array[index];
+// };
+let getElementAtIndex = (array, index) => array[index];
+let myArray = [10, 20, 30, 40, 50];
+let index = 3;
+
+let result = getElementAtIndex(array, index);
+
+console.log(result);

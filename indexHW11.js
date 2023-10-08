7689f7a80759555722d055c4f625c6268f5a7aa7
Egor Danilov <mellowd94@gmail.com>
2023-10-08 09:35:18 +0200

    add lesson 12

--- indexHW11.js
+++ indexHW11.js
@@ -10,13 +10,30 @@ const newCars = cars.map((car) => ({
   isDiesel: car.isDiesel,
 }));
 console.log(newCars);
-//2
+//2 Создайте новый массив, где оставьте только машины с дизельным двигателем.
 const dieselCars = cars.filter((car) => car.isDiesel);
 console.log(dieselCars);
-//3
+//3 Создайте новый массив, где оставьте только машины не с дизельным двигателем.
+const nonDieselCars = cars.filter((car) => !car.isDiesel);
+console.log(nonDieselCars);
+//4 Посчитайте общую стоимость всех машин не с дизельным двигателем.
+const sum = nonDieselCars.reduce((acc, el) => acc + el, 0,
+);
+console.log(sum);
+//5 Повысьте цену всех машин в массиве на 20%.
+cars.forEach((car) => (car.price = car.price * 1.2));
+//6 Создайте новый массив, где все дизельные машины заменены на { brand: "Tesla", price: 25000, isDiesel: false }
+const carsWithoutDiesel = cars.map(car => (car.isDiesel )); // тут не знаю
+console.log(carsWithoutDiesel);
 
-//4
 
-//5
+//7 ToDoList
 
-//6
+const toDoList = ['погулять', 'поспать','покодить','поработать',];
+toDoList.splice(1, 0, 'покушать');
+
+console.log(ToDoList);
+const newToDo = toDoList.slice();
+toDoList.splice(1, 2);
+console.log(newToDo);
+console.log(toDoList);
